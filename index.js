a63a1e6fd1d820ac306c529d8876b168bb4668b0
nanna29 <luna2907@naver.com>
2024-06-04 13:03:17 +0900

    fix

--- index.js
+++ index.js
@@ -14,16 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     scrollToTopBtn.style.display = "block";
   } else {
     scrollToTopBtn.style.display = "none";
   }
-}
+};
 
 scrollToTopLink2.addEventListener("click", function (e) {
   e.preventDefault();
