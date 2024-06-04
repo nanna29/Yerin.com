e56658e5f549cf6b9aa33ce991a195383b29cb44
nanna29 <luna2907@naver.com>
2024-06-04 12:57:26 +0900

    feat: Add move to top btn

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var currentUrl = window.location.href;
+const scrollToTopBtn = document.getElementById("toTopBtn");
+const scrollToTopLink2 = document.getElementById("scrollToTop2");
 
-  var navLinks = document.querySelectorAll(".nav-link");
+document.addEventListener("DOMContentLoaded", function () {
+  const currentUrl = window.location.href;
+
+  const navLinks = document.querySelectorAll(".nav-link");
 
   navLinks.forEach(function (link) {
     if (link.href === currentUrl) {
       link.classList.add("active");
     }
+  });
+});
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+scrollToTopLink2.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
   });
 });
--- letter/letter.js
+++ letter/letter.js
@@ -5,13 +5,10 @@ const modal = document.getElementById("Modal");
 const closeBtn = document.getElementsByClassName("closeBtn")[0];
 
 document.addEventListener("DOMContentLoaded", function () {
-  // 현재 URL 가져오기
   var currentUrl = window.location.href;
 
-  // 모든 nav-link 요소들을 가져오기
   var navLinks = document.querySelectorAll(".nav-link");
 
-  // nav-link 요소들을 순회하며 현재 URL과 비교
   navLinks.forEach(function (link) {
     if (link.href === currentUrl) {
       link.classList.add("active");
@@ -32,7 +29,6 @@ sendButton.addEventListener("click", () => {
   const inputs = document.querySelectorAll("input");
   const textareas = document.querySelectorAll("textarea");
 
-  // Check if any input or textarea is empty
   for (const input of inputs) {
     if (input.value.trim() === "") {
       isEmpty = true;
@@ -49,26 +45,20 @@ sendButton.addEventListener("click", () => {
     }
   }
 
-  // If any input or textarea is empty, show alert
   if (isEmpty) {
     alert("빈칸을 모두 작성해주세요.");
   } else {
-    // Show modal only when all input fields are filled
     modal.style.display = "block";
 
-    // When the user clicks on the close button, close the modal
     closeBtn.onclick = function () {
       modal.style.display = "none";
     };
 
-    // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
       if (event.target == modal) {
         modal.style.display = "none";
       }
     };
-
-    // Clear all input and textarea fields
     inputs.forEach((input) => (input.value = ""));
     textareas.forEach((textarea) => (textarea.value = ""));
   }
